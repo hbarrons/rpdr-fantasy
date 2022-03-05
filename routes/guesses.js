86b01a1bc4d27c4e265afe4f4c61363c39fd622a
hbarrons <hbarrons@gmail.com>
2022-03-05 17:29:16 -0500

    add some CSS to show page

---
+++ routes/guesses.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as guessesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from "../middleware/middleware.js";
+
+const router = Router()
+
+
+
+
+export {
+  router
+}
